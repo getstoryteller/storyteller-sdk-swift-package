0cac102ff30f2ab1eb3e7063056fba5c1a72c390
Piotr Debosz <piotr.debosz@gmail.com>
2022-09-21 19:56:35 +0200

    [Update] StorytellerSDK (8.1.13)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(name: "StorytellerSDK",
-                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/8.1.12/StorytellerSDK.zip",
-                    checksum: "4369bc6fd2f5fc14fc0c2eee8a064f3e0d57035a25f57f664aeab5c5f964266b")
+                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/8.1.13/StorytellerSDK.zip",
+                    checksum: "dba110b637f4a4761288abc8d58d4a63c9ffcaea85f464d5050be8d7ce51b091")
     ]
 )
