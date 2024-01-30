96390f8f5f1a1ed7610d85ad613e792a5178b4d6
Bartosz Stelmaszuk <stelmaszuk.b@gmail.com>
2024-01-30 19:07:47 +0100

    support tvOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "storyteller-sdk-package",
     platforms: [
-      .iOS(.v11)
+      .iOS(.v11),
+      .tvOS(.v15)
     ],
     products: [
         .library(
