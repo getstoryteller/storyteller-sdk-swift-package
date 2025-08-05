d8df14af9b36f834fccc22752dfecf212d89c5b6
runner <runner@Mac-1754421163933.local>
2025-08-05 19:47:05 +0000

    Updating Storyteller to 10.13.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -16,7 +15,7 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(name: "StorytellerSDK",
-                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/10.13.0-alpha.1/StorytellerSDK.zip",
-                    checksum: "0430775d9b7bf05d592f84c20a4dd284bc2c036a99c905f0fcc64f622da59636")
+                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/10.13.0/StorytellerSDK.zip",
+                    checksum: "23df8b5f7071fcd6304dd1dc9e130645390995c935f6bddedb8e0ae1c1ff6d48")
     ]
 )
