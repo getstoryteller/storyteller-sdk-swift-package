c531bf579720ed9eaa0687e21c946af6ec33fd5b
runner <runner@Mac-1764001602765.local>
2025-11-24 17:12:37 +0000

    Updating Storyteller to 11.0.5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(name: "StorytellerSDK",
-                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/11.0.4/StorytellerSDK.zip",
-                    checksum: "20a30b1ea709a4018714c6fe04310e7b12dfd5dd72cc4fcbf655b5a5019e0ec2")
+                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/11.0.5/StorytellerSDK.zip",
+                    checksum: "baadcffa431c9438dd55e08dd72880dbb26c1dcfd7f062f6819069eea8e16bf2")
     ]
 )
