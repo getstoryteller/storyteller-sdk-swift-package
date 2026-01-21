d8921b13983287fdf39c2d3c9510daf5b4a000a4
Marco Domingues <marco.domingues@getstoryteller.com>
2026-01-21 17:41:38 +0000

    Updating Storyteller to 11.2.0-alpha.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
       .binaryTarget(name: "StorytellerSDK",
-                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/11.2.0/StorytellerSDK.zip",
-                    checksum: "a983653b4b9abb103c5d72282049acd2e51f9d62485fde967ba87a30dcb57922")
+                    url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/11.2.0-alpha.1/StorytellerSDK.zip",
+                    checksum: "a322719a4bf4b37da6c9bc0c2fcd605b0171f5e57eb2dd2400a1ff0c513b3399")
     ]
 )
